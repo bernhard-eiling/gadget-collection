50da1db98ed32c3df068be08c79e9e84e0f77d20
Bernhard Eiling <bernhard@blinkist.com>
2014-11-21 21:32:36 +0100

    added users / gadget models, implemented account creation

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem 'spring',        group: :development
 
 gem 'rspec-rails'
 
+gem 'bcrypt'
+
+gem 'bootstrap-sass'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+
+  has_many :gadgets, dependent: :destroy
+
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  #validates :name, :email, presence: true
+  #validates :email, format: {with: VALID_EMAIL_REGEX}
+  #validates_uniqueness_of :email, {case_sensitive: false}
+  #validates_length_of :password, {minimum: 5}
+
+end
